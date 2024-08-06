bbaa0a4b63dd0a977e2192d744673cbebd1049f2
Luís Soares <gersonluis1209@gmail.com>
2024-08-06 02:02:40 -0300

    Subindo a versão ruim do código

--- src/application/Program.java
+++ src/application/Program.java
@@ -53,6 +53,7 @@ public class Program {
 			else {
 				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: "+reservation);
+				System.out.println();
 				
 				
 				
